46ebf87c66abc8a65ba19c424f9f5d5195992805
miguelx46 <carrizosam@uninorte.edu.co>
2025-03-17 15:53:06 -0500

    clase Cat creada

---
+++ src/core/Cat.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author migue
+ */
+public class Cat {
+    String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+    
+}
