09dba417d0b377d8c6fc235ed9c3b8d0787853e0
miguelx46 <carrizosam@uninorte.edu.co>
2025-03-17 15:46:27 -0500

    clase Dog creada

---
+++ src/core/Dog.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author migue
+ */
+public class Dog {
+    String nombre;
+    String raza;
+    boolean tieneHijos;
+
+    public Dog(String nombre, String raza, boolean tieneHijos) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.tieneHijos = tieneHijos;
+    }
+    
+}
