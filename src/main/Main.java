db43c0379f1e0b6d85e6f6199a43a79efa94f319
miguelx46 <carrizosam@uninorte.edu.co>
2025-03-17 15:29:47 -0500

    added person class

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import core.Person;
+
+/**
+ *
+ * @author migue
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person(123, "Pepe", "19");
+        System.out.println(person);
+    }
+}
